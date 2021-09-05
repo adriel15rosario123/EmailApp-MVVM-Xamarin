ac3ad764edce112fa203416e2ff6f1d8d6b07491
AdrielRosario <adriel15rosario@gmail.com>
2021-09-05 11:06:30 -0400

    nuevas imagenes y vista

--- MvvmSample2/MvvmSample2/ViewModels/InboxViewModel.cs
+++ MvvmSample2/MvvmSample2/ViewModels/InboxViewModel.cs
@@ -12,19 +12,28 @@ namespace MvvmSample2.ViewModels
 {
     public class InboxViewModel: BaseViewModel
     {
-        public ObservableCollection<Email> Inbox { get; set; } = new ObservableCollection<Email>() { 
-            
-            new Email("Adriel","Supermercado Bravo","tarea 1","adknfasjndasbdjbasjkdbfjkasbdasdfansdjlfasdfasjdsadfsa",DateTime.Now.ToString("MMM dd"),"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"),
-            new Email("Adriel Rosario","Banco Popular","tarea 2","adknfasjndasbdjbasjkdbfjkasbdasdfansdjlfasdfasjdsadfsa",DateTime.Now.ToString("MMM dd"),"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"),
-            new Email("Alex","Banco Popular","tarea 3","adknfasjndasbdjbasjkdbfjkasbdasdfansdjlfasdfasjdsadfsa",DateTime.Now.ToString("MMM dd"),"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"),
-            new Email("Raul Lopez","Libreria ","tarea 4","adknfasjndasbdjbasjkdbfjkasbdasdfansdjlfasdfasjdsadfsa",DateTime.Now.ToString("MMM dd"),"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"),
-            new Email("Yudelka Sanchez","Adriel Rosario","tarea 5","adknfasjndasbdjbasjkdbfjkasbdasdfansdjlfasdfasjdsadfsa",DateTime.Now.ToString("MMM dd"),"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"),
-            new Email("Carlos Diaz","Juan Lopez","tarea 6","adknfasjndasbdjbasjkdbfjkasbdasdfansdjlfasdfasjdsadfsa",DateTime.Now.ToString("MMM dd"),"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"),
-            new Email("Libreria","La Sirena","tarea 7","adknfasjndasbdjbasjkdbfjkasbdasdfansdjlfasdfasjdsadfsa",DateTime.Now.ToString("MMM dd"),"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"),
+        public ObservableCollection<Email> Inbox { get; set; } = new ObservableCollection<Email>();
 
+        private Email _selectedEmail;
 
+        public Email SelectedEmail
+        {
+            get
+            {
+                return _selectedEmail;
+            }
 
-        };
+            set
+            {
+                _selectedEmail = value;
+
+                if(_selectedEmail != null)
+                {
+                    ShowEmailDetailPageCommand.Execute(_selectedEmail);
+                    //SelectedEmail = null;
+                }
+            }
+        }
 
         public ICommand ComposeNewEmailCommand { get; }
         public ICommand DeleteEmailCommnad { get; }
